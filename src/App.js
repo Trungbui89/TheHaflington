8f3bccc4ab390c4aac42220a2a9af4185875d76d
trungbui89 <2104799703@qq.com>
2022-04-02 17:48:54 +0700

    creat first layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom'
+import Main from './components/MainComponent';
 import './App.css';
-class App extends Component {
 
-  render() {
-    return (
+function App()  {
+
+  return (
+    <BrowserRouter>
       <div className="App">
+        <Main />
       </div>
-    );
-  }
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ src/components/MainComponent.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Header from './layout/Header';
+import Footer from './layout/Footer';
+import Home from './layout/Home';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+function Main() {
+
+    const HomePage = () => {
+        return (
+            <Home />
+        )
+    }
+
+    return (
+        <div>
+            <Header />
+                <Switch>
+                    <Route path='/home' component={HomePage} />
+                    <Redirect to="/home" />
+                </Switch>
+            <Footer />
+        </div>
+    )
+}
+
+export default Main
