328e1fb193e7b501926ca6a0b445c4de2b4f5cc4
trungbui89 <2104799703@qq.com>
2022-04-03 10:50:57 +0700

    add about us

---
+++ src/components/layout/home/About.js
@@ -0,0 +1,29 @@
+import React from "react"
+
+function About() {
+    return (
+        <div className="about">
+            <div className="about-header-container">
+                <div className="about-title-top">
+                    <p>ABOUT</p>
+                </div>
+                <div className="about-title-bot">
+                    <p>YOUR NEIGHBORHOOD</p>
+                    <p>INTRODUCTION</p>
+                </div>
+                <div className="about-content">
+                    <p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p>
+                </div>
+                <div className="about-arrow">
+                    <svg preserveAspectRatio="xMidYMid meet" data-bbox="45.87 4.75 111.491 192.633" xmlns="http://www.w3.org/2000/svg" viewBox="45.87 4.75 111.491 192.633">
+                        <g>
+                            <path d="M156.124 46.421L103.09 5.431a3.245 3.245 0 0 0-4.003.019L47.109 46.44a3.252 3.252 0 0 0-.54 4.568c1.112 1.411 3.233 1.655 4.644.54L98 14.711V194.13c0 1.798 1.202 3.253 3 3.253s3-1.455 3-3.253V14.63l47.969 36.937c.591.457 1.379.68 2.077.68.972 0 1.976-.435 2.618-1.264 1.1-1.42.883-3.463-.54-4.562z"></path>
+                        </g>
+                    </svg>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About
--- src/components/layout/home/Home.js
+++ src/components/layout/home/Home.js
@@ -1,39 +1,42 @@
 import React, { useEffect } from 'react';
+import About from './About';
 
 function Home() {
 
     const d = new Date()
 
     useEffect(() => {
-      const parallax = document.querySelector('.home-container .top-bg-img img')
+      const parallax = document.querySelector('.home-container .top-bg-img')
       window.addEventListener('scroll', function() {
         let offset = window.scrollY
-        parallax.style.objectPosition = `0, ${offset * 0.1}px`
+        parallax.style.backgroundPositionY = `${offset * 0.6}px`
       })
     },[])
   
     return(
       <div className="home-container">
         <div className='top-bg-bar'>
-          <div className='top-bg-img' />
-          <div className='brand-card'>
-            <div className='brand-card-container'>
-              <div className='info-top'>
-                <p>HÀ NỘI</p>
-                <p>|</p>
-                <p>{d.getFullYear()}</p>
-              </div>
-              <div className='info-middle'>
-                <p>the</p>
-                <p>HAFLINGTON</p>
-              </div>
-              <div className='info-bot'>
-                <p>94 Hàng Mã, Hoàn Kiếm, Hà Nội</p>
-                <p>Mở cửa: 9AM - 9PM</p>
+          <div className='top-bg-img'>
+            <div className='brand-card'>
+              <div className='brand-card-container'>
+                <div className='info-top'>
+                  <p>Hà Nội</p>
+                  <p> | </p>
+                  <p>{d.getFullYear()}</p>
+                </div>
+                <div className='info-middle'>
+                  <p>the</p>
+                  <p>Haflington</p>
+                </div>
+                <div className='info-bot'>
+                  <p>94 Hàng Mã, Hoàn Kiếm, Hà Nội</p>
+                  <p>Mở cửa: 9AM - 9PM</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
+        <About />
       </div>
     )
 }
