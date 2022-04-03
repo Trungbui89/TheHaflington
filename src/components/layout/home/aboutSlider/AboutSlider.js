cc6968935cfb0ea0d6134e7b4e21f92f06cb7ff3
trungbui89 <2104799703@qq.com>
2022-04-04 01:20:55 +0700

    side effect to about us

--- src/components/layout/home/aboutSlider/AboutSlider.js
+++ src/components/layout/home/aboutSlider/AboutSlider.js
@@ -1,23 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import aboutSlideSideEffect from "./aboutSlideSideEffect";
 
-function AboutSlider() {    
+function AboutSlider() {   
+    
+    useEffect(() => {
+        aboutSlideSideEffect()
+    },[])
 
     return (
         <div className="about-slide-container">
             <div className="slide-body row">
                 <div className="cardList col-12 col-md-8 col-lg-8">
-                    {/* <button className="silde-btn-left">
-                        <div className="icon">
-                            <svg>
-                                <symbol id="arrow-left" xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'>
-                                    <polyline points='328 112 184 256 328 400' />
-                                </symbol>
-                            </svg>
+
+                    <button className="slide-btn-left">
+                        <div className="slide-icon">
+                            <i className="fa fa-chevron-left" aria-hidden="true"></i>
                         </div>
-                    </button> */}
+                    </button>
 
                     <div className="slide-content">
-                        <div className="slide-card-items">
+                        <div className="slide-card-items preview">
                             <div className="card-image">
                                 <img src="/assets/images/slider/image-1.jpg" alt="" />
                             </div>
@@ -29,42 +31,38 @@ function AboutSlider() {
                             </div>
                         </div>
 
-                        <div className="slide-card-items">
+                        <div className="slide-card-items next">
                             <div className="card-image">
                                 <img src="/assets/images/slider/image-3.jpg" alt="" />
                             </div>
                         </div>
                     </div>
 
-                    {/* <button className="slide-btn-right">
-                        <div className="icon">
-                            <svg>
-                                <symbol id="arrow-right" xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'>
-                                    <polyline points='184 112 328 256 184 400' />
-                                </symbol>
-                            </svg>
+                    <button className="slide-btn-right">
+                        <div className="slide-icon">
+                            <i className="fa fa-chevron-right" aria-hidden="true"></i>
                         </div>
-                    </button> */}
+                    </button>
                 </div>
 
                 <div className="infoList col-12 col-md-4 col-lg-4">
-                    <div className="info__wrslide-bodyer">
-                        <div className="info current--info">
-                            <h1 className="text name">Highlands</h1>
-                            <h4 className="text location">Scotland</h4>
-                            <p className="text description">The mountains are calling</p>
+                    <div className="silde-info-body active">
+                        <div className="silde-info-item">
+                            <p className="silde-info-top-text">HAPPY HOUR</p>
+                            <p className="silde-info-title">CRAFT BEER TUESDAY</p>
+                            <p className="silde-info-words">The I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p>
                         </div>
 
-                        <div className="info next--info">
-                            <h1 className="text name">Machu Pichu</h1>
-                            <h4 className="text location">Peru</h4>
-                            <p className="text description">Adventure is never far away</p>
+                        <div className="silde-info-item active">
+                            <p className="silde-info-top-text">HAPPY HOUR 2</p>
+                            <p className="silde-info-title">CRAFT BEER TUESDAY</p>
+                            <p className="silde-info-words">I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p>
                         </div>
 
-                        <div className="info previous--info">
-                            <h1 className="text name">Chamonix</h1>
-                            <h4 className="text location">France</h4>
-                            <p className="text description">Let your dreams come true</p>
+                        <div className="silde-info-item">
+                            <p className="silde-info-top-text">HAPPY HOUR 3</p>
+                            <p className="silde-info-title">CRAFT BEER TUESDAY</p>
+                            <p className="silde-info-words">I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p>
                         </div>
                     </div>
                 </div>
--- src/components/layout/home/aboutSlider/aboutSlideSideEffect.js
+++ src/components/layout/home/aboutSlider/aboutSlideSideEffect.js
@@ -1,6 +1,99 @@
+function aboutSlideSideEffect() {
 
-// function aboutSlideSideEffect() {
-    
-// }
+    const btnLeft = document.querySelector('.slide-btn-left')
+    const btnRight = document.querySelector('.slide-btn-right')
 
-// export default aboutSlideSideEffect
+    const slideItems = document.querySelectorAll('.slide-card-items')
+    const infoBorder = document.querySelector('.silde-info-body')
+    const slideInfos = document.querySelectorAll('.silde-info-item')
+
+    btnRight.addEventListener('click', turnRight)
+    btnLeft.addEventListener('click', turnLeft)
+
+    function disableBtn() {
+        btnLeft.setAttributeNode(document.createAttribute('disabled'))
+        btnRight.setAttributeNode(document.createAttribute('disabled'))
+        setTimeout(() => {
+            btnLeft.removeAttribute('disabled')
+            btnRight.removeAttribute('disabled')
+        },800)
+    }
+
+    function infoBorderFade() {
+        infoBorder.classList.remove('active')
+        setTimeout(() => {infoBorder.classList.add('active')},100)
+    }
+
+    function turnLeft() {
+        const slideLenght = slideItems.length
+        disableBtn()
+        infoBorderFade()
+
+        for(let i = 0; i < slideLenght;) {
+                if(slideItems[i].className === 'slide-card-items active') {
+                    slideItems[i].className = 'slide-card-items next'
+                    slideInfos[i].classList.remove('active')
+
+
+                if(slideLenght-1 === i) {
+                    slideItems[0].className = 'slide-card-items preview'
+                    slideItems[i-1].className = 'slide-card-items active'
+                    slideInfos[i-1].classList.add('active')
+                    break
+
+                } else if(i === 0) {
+                    slideItems[i+1].className = 'slide-card-items preview'
+                    slideItems[slideLenght-1].className = 'slide-card-items active'
+                    slideInfos[slideLenght-1].classList.add('active')
+
+                    break
+
+                } else {
+                    slideItems[i+1].className = 'slide-card-items preview'
+                    slideItems[i-1].className = 'slide-card-items active'
+                    slideInfos[i-1].classList.add('active')
+
+                    break
+                }
+            } else i++
+        }
+    }
+
+    function turnRight() {
+        const slideLenght = slideItems.length
+        disableBtn()
+        infoBorderFade()
+        
+        for(let i = 0; i < slideLenght;) {
+            if(slideItems[i].className === 'slide-card-items active') {
+                slideItems[i].className = 'slide-card-items preview'
+                slideInfos[i].classList.remove('active')
+
+
+                if(slideLenght-1 === i) {
+                    slideItems[0].className = 'slide-card-items active'
+                    slideItems[i-1].className = 'slide-card-items next'
+                    slideInfos[0].classList.add('active')
+
+                    break
+
+                } else if(i === 0) {
+                    slideItems[i+1].className = 'slide-card-items active'
+                    slideItems[slideLenght-1].className = 'slide-card-items next'
+                    slideInfos[i+1].classList.add('active')
+
+                    break
+
+                } else {
+                    slideItems[i+1].className = 'slide-card-items active'
+                    slideItems[i-1].className = 'slide-card-items next'
+                    slideInfos[i+1].classList.add('active')
+
+                    break
+                }
+            } else i++
+        }
+    }
+}
+
+export default aboutSlideSideEffect
