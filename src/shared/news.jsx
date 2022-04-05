2e66901755570044a5b506854c951c217b86639a
trungbui89 <2104799703@qq.com>
2022-04-05 22:37:17 +0700

    add shared

---
+++ src/shared/news.jsx
@@ -0,0 +1,87 @@
+export const news =
+    [
+        {
+            id: "0",
+            name: " Gin Journeys",
+            day: '16',
+            month: 'feb',
+            year: '22'
+        },
+        {
+            id: "1",
+            name: " Sundays at ATLAS",
+            day: '16',
+            month: 'feb',
+            year: '22'
+        },
+        {
+            id: "2",
+            name: " ATLAS Afternoon Tea",
+            day: '16',
+            month: 'feb',
+            year: '22'
+        },
+        {
+            id: "3",
+            name: "Virtual ATLAS Masterclass",
+            day: '16',
+            month: 'feb',
+            year: '22'
+        },
+        {
+            id: "4",
+            name: "ATLAS Aperitivo",
+            day: '16',
+            month: 'feb',
+            year: '22'
+        },
+        {
+            id: "5",
+            name: "ATLAS at Home - Takeaway menu",
+            day: '16',
+            month: 'feb',
+            year: '22'
+        },
+        {
+            id: "6",
+            name: " Juniper Society goes Virtual",
+            day: '16',
+            month: 'feb',
+            year: '22'
+        },
+        {
+            id: "7",
+            name: "#ATLASatHome",
+            day: '16',
+            month: 'feb',
+            year: '22'
+        },
+        {
+            id: "8",
+            name: "ATLAS Take-away",
+            day: '16',
+            month: 'feb',
+            year: '22'
+        },
+        {
+            id: "9",
+            name: "St George ATLAS Orange Gin",
+            day: '16',
+            month: 'feb',
+            year: '22'
+        },
+        {
+            id: "10",
+            name: "St New Year's Eve Party 2020",
+            day: '16',
+            month: 'feb',
+            year: '22'
+        },
+        {
+            id: "11",
+            name: "St TIME's World's Greatest Places (Aug 18)",
+            day: '16',
+            month: 'feb',
+            year: '22'
+        }
+    ]
