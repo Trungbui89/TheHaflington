017c0dbdd275d0a1e7d522f22828b40d75924220
trungbui89 <2104799703@qq.com>
2022-04-08 15:33:11 +0700

    update img

--- src/components/layout/menu/Menu.js
+++ src/components/layout/menu/Menu.js
@@ -1,7 +1,26 @@
 import React from "react";
 
 function Menu() {
-    return <div></div>
+    return (
+        <div className="menu-page">
+            <div className="menu-title">
+                <div className="menu-title-container" >
+                    <p>the</p>
+                    <p>Haflington</p>
+                    <p>menu</p>
+                    <p>scroll down</p>
+                    <div className="menu-arrow" />
+                </div>
+                <div className="menu-title-over-color" />
+            </div>
+            <div className="menu-1">
+                <div className="menu-1-bg" />
+                <div className="menu-1-content">
+
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Menu
