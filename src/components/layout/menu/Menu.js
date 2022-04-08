416f2a7cf5928f27340da1a6f6160d6b40f61b7f
trungbui89 <2104799703@qq.com>
2022-04-08 18:28:29 +0700

    and menu item 1

--- src/components/layout/menu/Menu.js
+++ src/components/layout/menu/Menu.js
@@ -16,8 +16,61 @@ function Menu() {
             <div className="menu-1">
                 <div className="menu-1-bg" />
                 <div className="menu-1-content">
-                    <div className="menu-1-img">
-                        
+                    <div className="menu-1-info">
+                        <div className="menu-1-group">
+                            <img src="https://raw.githubusercontent.com/Trungbui89/TheHaflington/main/public/assets/images/menu/Cocktails-menu.png" />
+                            <div className="menu-1-title">
+                            <h1>COCKTAILS OF THE 1650-1800</h1>
+                            <h1>PUNCH ERA</h1>
+                            </div>
+                            <div className="menu-1-items">
+                                <div className="item-1">
+                                    <div className="item-1-icon">
+                                        <img src="https://raw.githubusercontent.com/Trungbui89/TheHaflington/main/public/assets/icon/items/Cocktails-item.png" />
+                                    </div>
+                                    <div className="top">
+                                        <h2>FISH HOUSE PUNCH</h2>
+                                        <h2>$23.00</h2>
+                                    </div>
+                                    <div className="mid">
+                                        <p>Mount Gay Black Barrel - Rémy Martin VSOP - Peach Infused Delord Blanch Armagnac - Lemon juice - Gomme</p>
+                                    </div>
+                                    <div className="bot">
+                                        <p>Tall and Sweet with big stones, just like George Washington.</p>
+                                    </div>
+                                </div>
+                                <div className="item-1">
+                                    <div className="item-1-icon">
+                                        <img src="https://raw.githubusercontent.com/Trungbui89/TheHaflington/main/public/assets/icon/items/Cocktails-item.png" />
+                                    </div>
+                                    <div className="top">
+                                        <h2>FISH HOUSE PUNCH</h2>
+                                        <h2>$23.00</h2>
+                                    </div>
+                                    <div className="mid">
+                                        <p>Mount Gay Black Barrel - Rémy Martin VSOP - Peach Infused Delord Blanch Armagnac - Lemon juice - Gomme</p>
+                                    </div>
+                                    <div className="bot">
+                                        <p>Tall and Sweet with big stones, just like George Washington.</p>
+                                    </div>
+                                </div>
+                                <div className="item-1">
+                                    <div className="item-1-icon">
+                                        <img src="https://raw.githubusercontent.com/Trungbui89/TheHaflington/main/public/assets/icon/items/Cocktails-item.png" />
+                                    </div>
+                                    <div className="top">
+                                        <h2>FISH HOUSE PUNCH</h2>
+                                        <h2>$23.00</h2>
+                                    </div>
+                                    <div className="mid">
+                                        <p>Mount Gay Black Barrel - Rémy Martin VSOP - Peach Infused Delord Blanch Armagnac - Lemon juice - Gomme</p>
+                                    </div>
+                                    <div className="bot">
+                                        <p>Tall and Sweet with big stones, just like George Washington.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
